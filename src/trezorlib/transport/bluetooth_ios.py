a7e37bd63dc2b9a9f122782a3ddb20ad5a7a487a
liyanhrxy <sweepmonkli@gmail.com>
2021-01-27 12:12:43 +0800

    optimize ble communication logic

--- src/trezorlib/transport/bluetooth_ios.py
+++ src/trezorlib/transport/bluetooth_ios.py
@@ -6,14 +6,16 @@ from typing import Iterable
 from .protocol import ProtocolBasedTransport, Handle, ProtocolV1
 from rubicon.objc import ObjCClass
 from electrum.i18n import _
+import threading
 
 BleHandler = ObjCClass("OKBlueManager")
+WRITE_SUCCESS = True
+IS_CANCEL = False
+lock = threading.RLock()
 
 
 class BlueToothIosHandler(Handle):
     BLE = BleHandler.sharedInstance()
-    WRITE_SUCCESS = True
-    IS_CANCEL = False
     WRITE_TIMEOUT = 5
     READ_TIMEOUT = 120
     RESPONSE = ""
@@ -29,50 +31,61 @@ class BlueToothIosHandler(Handle):
 
     @classmethod
     def set_cancel_flag(cls) -> None:
-        cls.IS_CANCEL = True
+        global IS_CANCEL
+        with lock:
+            IS_CANCEL = True
 
     @classmethod
     def set_write_success_flag(cls) -> None:
-        cls.WRITE_SUCCESS = True
+        global WRITE_SUCCESS
+        with lock:
+            WRITE_SUCCESS = True
 
     @classmethod
-    def set_response(cls, response: str):
-        cls.RESPONSE = response
+    def set_response(cls, response: str) -> None:
+        with lock:
+            cls.RESPONSE = response
 
     @classmethod
     def write_chunk(cls, chunk: bytes) -> None:
+        global WRITE_SUCCESS, IS_CANCEL
         assert cls.BLE is not None, _("Bluetooth device not available")
         chunks = binascii.unhexlify(bytes(chunk).hex())
         cls.RESPONSE = ''
-        cls.IS_CANCEL = False
+        IS_CANCEL = False
         start = int(time.time())
-        while not cls.IS_CANCEL:
+        while not IS_CANCEL:
             wait_seconds = int(time.time()) - start
-            if cls.WRITE_SUCCESS and not cls.IS_CANCEL:
-                cls.WRITE_SUCCESS = False
-                cls.BLE.characteristicWrite(chunks)
-            elif wait_seconds >= cls.WRITE_TIMEOUT:
-                raise BaseException(_("Waiting for send timeout"))
-            else:
-                time.sleep(0.001)
-        if cls.IS_CANCEL:
+            with lock:
+                if WRITE_SUCCESS and not IS_CANCEL:
+                    cls.BLE.characteristicWrite(chunks)
+                    WRITE_SUCCESS = False
+                    return
+                elif wait_seconds >= cls.WRITE_TIMEOUT:
+                    WRITE_SUCCESS = True
+                    raise BaseException(_("Waiting for send timeout"))
+                else:
+                    time.sleep(0.001)
+        if IS_CANCEL:
             raise BaseException("user cancel")
 
     @classmethod
     def read_ble(cls) -> bytes:
+        global IS_CANCEL
         start = int(time.time())
-        cls.IS_CANCEL = False
-        while not cls.IS_CANCEL:
+        IS_CANCEL = False
+        while not IS_CANCEL:
             wait_seconds = int(time.time()) - start
-            if cls.RESPONSE and not cls.IS_CANCEL:
-                new_response = bytes(binascii.unhexlify(str(cls.RESPONSE)))
-                cls.RESPONSE = ''
-                return new_response
-            elif wait_seconds >= cls.READ_TIMEOUT:
-                raise BaseException("read ble response timeout")
-            else:
-                time.sleep(0.1)
-        if cls.IS_CANCEL:
+            with lock:
+                if cls.RESPONSE and not IS_CANCEL:
+                    new_response = bytes(binascii.unhexlify(str(cls.RESPONSE)))
+                    cls.RESPONSE = ''
+                    return new_response
+                elif wait_seconds >= cls.READ_TIMEOUT:
+                    raise BaseException("read ble response timeout")
+                else:
+                    time.sleep(0.1)
+        if IS_CANCEL:
             cls.RESPONSE = ''
             raise BaseException("user cancel")
 
